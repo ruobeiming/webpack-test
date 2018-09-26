f7f856fb9a25a559a90eb619e4e91fd54f4e6530
Helen Wang <wangh@cn.ibm.com>
2018-09-26 09:51:16 +0800

    init

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+	<div>
+		{{aaa}}
+	</div>
+</template>
+
+<script type="text/javascript">
+	 export default {
+		data() {
+			return {
+				aaa: 'abc'
+			}
+		}
+	}
+</script>
+<style lang="less" scoped>
+	div {
+		color: red;
+	}
+</style>
